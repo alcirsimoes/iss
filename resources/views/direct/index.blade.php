c3f5e6f495f14abb33076f3d2e726915ab31d9d2
Alcir S. Oliveira <alcir.simoes@criarenet.net>
2017-10-18 10:11:13 -0200

    Init

--- resources/views/direct/index.blade.php
+++ resources/views/direct/index.blade.php
@@ -5,51 +5,19 @@
     <div class="row">
         <div class="col-md-12">
             <div class="panel panel-default">
-                <div class="panel-heading">{{ $survey->name }} - {{ $sample->name }}</div>
-
-                <div class="panel-body">
-                    <a href="{{ route('subject.create', ['id'=>$survey->samples->first()->id,'r'=>1]) }}" class="btn btn-default">Nova entrada</a>
-                    <h5>Responder para:</h5>
-
-                    <table class="table table-condensed">
-                        <thead>
-                            <tr>
-                                <!-- <th>id</th> -->
-                                <th>nome</th>
-                                <th>empresa</th>
-                                <th>telefone</th>
-                                <th>Iniciar Pesquisa</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @if (Auth::user()->isSupervisor || Auth::user()->isAdmin)
-                                @forelse ($sample->subjects as $subject)
-                                    <tr>
-                                        <!-- <td>{{ $subject->id }}</td> -->
-                                        <td><a href="{{ route('subject.edit', [$subject->id]) }}">{{ $subject->name }}</a></td>
-                                        <td>{{ $subject->company }}</td>
-                                        <td>{{ $subject->telephone }}</td>
-                                        <td><a href="{{ route('form.create', [$sample->id, $subject->id]) }}" class="btn btn-default">Responder </a></td>
-                                    </tr>
-                                @empty
-                                @endforelse
-                            @else
-                                @forelse($sample->subjects->where('user_id', Auth::user()->id) as $subject)
-                                <tr>
-                                    <!-- <td>{{ $subject->id }}</td> -->
-                                    <td><a href="{{ route('subject.edit', [$subject->id]) }}">{{ $subject->name }}</a></td>
-                                    <td>{{ $subject->company }}</td>
-                                    <td>{{ $subject->telephone }}</td>
-                                    <td><a href="{{ route('form.create', [$sample->id, $subject->id]) }}" class="btn btn-default">Responder </a></td>
-                                </tr>
-                                @empty
-                                @endforelse
-                            @endif
-
-                        </tbody>
-                    </table>
-                </div>
+                <div class="panel-heading">{{ $survey->name }} - {{ $sample->name }} - {{ $subject->name }}</div>
             </div>
+
+            <div class="jumbotron">
+                <p class="lead">{!! $survey->intro !!}</p>
+            </div>
+
+            <form action="{{ route('form.next') }}" method="post">
+                {{ csrf_field() }}
+                <input type="hidden" name="init" value="1">
+                <input type="hidden" name="next" value="0">
+                <button type="submit" class="btn btn-primary">Começar</button>
+            </form>
         </div>
     </div>
 </div>
